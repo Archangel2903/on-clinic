6be56c3e7ec6e39ce3d72a9e3d290dc092e0af8a
Dmitriy <archangel2903@gmail.com>
2019-05-08 19:21:51 +0300

    edits

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,84 @@ import 'leaflet/dist/images/marker-icon-2x.png';
 import 'leaflet/dist/images/marker-icon.png';
 import 'leaflet/dist/images/marker-shadow.png';
 
+/****/
+(function () {
+
+    function isInt(num) {
+        //作用:是否为整数
+        //返回:true是 false否
+        var res = false;
+        try {
+            if (String(num).indexOf(".") == -1 && String(num).indexOf(",") == -1) {
+                res = parseInt(num) % 1 === 0 ? true : false;
+            }
+        } catch (e) {
+            res = false;
+        }
+        return res;
+    }
+
+    function isFloat(num) {
+        //作用:是否为小数
+        //返回:小数位数(-1不是小数)
+        var res = -1;
+        try {
+            if (String(num).indexOf(".") != -1) {
+                var index = String(num).indexOf(".") + 1; //获取小数点的位置
+                var count = String(num).length - index; //获取小数点后的个数
+                if (index > 0) {
+                    res = count;
+                }
+            }
+        } catch (e) {
+            res = -1;
+        }
+        return res;
+    }
+
+    $.fn.numScroll = function (options) {
+
+        var settings = $.extend({
+            'time': 1500,
+            'delay': 0
+        }, options);
+
+        return this.each(function () {
+            var $this = $(this);
+            var $settings = settings;
+
+            var num = $this.attr("data-num") || $this.text(); //实际值
+            var temp = 0; //初始值
+            $this.text(temp);
+            var numIsInt = isInt(num);
+            var numIsFloat = isFloat(num);
+            var step = (num / $settings.time) * 10; //步长
+
+            setTimeout(function () {
+                var numScroll = setInterval(function () {
+                    if (numIsInt) {
+                        $this.text(Math.floor(temp));
+                    } else if (numIsFloat != -1) {
+                        $this.text(temp.toFixed(numIsFloat));
+                    } else {
+                        $this.text(num);
+                        clearInterval(numScroll);
+                        return;
+                    }
+                    temp += step;
+                    if (temp > num) {
+                        $this.text(num);
+                        clearInterval(numScroll);
+                    }
+                }, 1);
+            }, $settings.delay);
+
+        });
+    };
+
+})($);
+/****/
+
 $(window).on('load', function () {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
         $('body').addClass('ios');
@@ -16,6 +94,45 @@ $(window).on('load', function () {
     }
 
     $('body').removeClass('loaded');
+
+
+    /****************************************************************************************/
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+        iconRetinaUrl: 'img/marker-icon-2x.png',
+        iconUrl: 'img/marker-icon.png',
+        shadowUrl: 'img/marker-shadow.png',
+    });
+
+    const map = L.map('map');
+    const defaultCenter = function () {
+        if (window.innerWidth < 768) {
+            return [43.262990, 76.931635];
+        } else {
+            return [43.244010, 76.967512];
+        }
+    };
+
+    let clinic1 = L.marker([43.23931, 76.94392]).addTo(map).bindPopup('г. Алматы, Проспект Абая, 20/14'),
+        clinic2 = L.marker([43.22795, 76.9069]).addTo(map).bindPopup('г. Алматы, улица Габдуллина 94А угол Ауэзова'),
+        clinic3 = L.marker([43.25944, 76.95952]).addTo(map).bindPopup('г. Алматы, улица Абдуллиных 30');
+
+    const basemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}');
+
+    basemap.addTo(map);
+
+    setTimeout(function () {
+        map.setView(defaultCenter(), 13, {
+            scrollWheelZoom: false
+        }).scrollWheelZoom.disable();
+    }, 1000);
+    /****************************************************************************************/
+
+    $('.num').each(function () {
+        $(this).numScroll({
+            time: 2500
+        });
+    });
 });
 
 $(window).on('load resize', function () {
@@ -27,29 +144,8 @@ $(window).on('load resize', function () {
 });
 
 $(function () {
-    /****************************************************************************************/
-    /* This code is needed to properly load the images in the Leaflet CSS */
-    delete L.Icon.Default.prototype._getIconUrl;
-    L.Icon.Default.mergeOptions({
-        iconRetinaUrl: 'img/marker-icon-2x.png',
-        iconUrl: 'img/marker-icon.png',
-        shadowUrl: 'img/marker-shadow.png',
-    });
-    const map = L.map('map');
-    const defaultCenter = function () {
-        if (window.innerWidth < 768) {
-            return [43.262990, 76.931635];
-        } else {
-            return [43.244010, 76.967512];
-        }
-    };
-    const defaultZoom = 13;
-    const basemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}');
-    let clinic1 = L.marker([43.23931, 76.94392]).addTo(map).bindPopup('г. Алматы, Проспект Абая, 20/14'),
-        clinic2 = L.marker([43.22795, 76.9069]).addTo(map).bindPopup('г. Алматы, улица Габдуллина 94А угол Ауэзова'),
-        clinic3 = L.marker([43.25944, 76.95952]).addTo(map).bindPopup('г. Алматы, улица Абдуллиных 30');
 
-    map.setView(defaultCenter(), defaultZoom);
-    basemap.addTo(map);
-    /****************************************************************************************/
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+    });
 });
